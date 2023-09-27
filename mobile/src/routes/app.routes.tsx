9acb6d11cf2ca85dc33325537cad4ece4b9834b5
Wladimir Ferreira Gonçalves <codewlad@gmail.com>
2023-09-27 09:20:33 -0300

    Tab navigation configured

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,33 +1,103 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'native-base';
+import {
+	createBottomTabNavigator,
+	BottomTabNavigationProp,
+} from '@react-navigation/bottom-tabs';
 
 import { Home } from '@screens/Home';
 import { Categories } from '@screens/Categories';
 import { Resume } from '@screens/Resume';
 import { Profile } from '@screens/Profile';
 
-const { Navigator, Screen } = createBottomTabNavigator();
+import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+type AppRoutes = {
+	home: undefined;
+	categories: undefined;
+	resume: undefined;
+	profile: undefined;
+};
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
+	const { sizes, colors } = useTheme();
+
+	const iconSize = sizes[6];
+
 	return (
-		<Navigator>
+		<Navigator
+			screenOptions={{
+				headerShown: false,
+				tabBarShowLabel: false,
+				tabBarActiveTintColor: colors.amber[400],
+				tabBarInactiveTintColor: colors.gray[500],
+				tabBarStyle: {
+					backgroundColor: colors.gray[900],
+					borderTopWidth: 0,
+					height: 96,
+					paddingTop: sizes[9],
+					paddingBottom: sizes[9],
+				},
+			}}
+		>
 			<Screen
 				name='home'
 				component={Home}
+				options={{
+					tabBarIcon: ({ color }) => (
+						<FontAwesome
+							name='home'
+							size={iconSize}
+							color={color}
+						/>
+					),
+				}}
 			/>
 
 			<Screen
 				name='categories'
 				component={Categories}
+				options={{
+					tabBarIcon: ({ color }) => (
+						<FontAwesome
+							name='list-ul'
+							size={iconSize}
+							color={color}
+						/>
+					),
+				}}
 			/>
 
 			<Screen
 				name='resume'
 				component={Resume}
+				options={{
+					tabBarIcon: ({ color }) => (
+						<FontAwesome5
+							name='coins'
+							size={iconSize}
+							color={color}
+						/>
+					),
+				}}
 			/>
 
 			<Screen
 				name='profile'
 				component={Profile}
+				options={{
+					tabBarIcon: ({ color }) => (
+						<FontAwesome
+							name='user-circle-o'
+							size={iconSize}
+							color={color}
+						/>
+					),
+				}}
 			/>
 		</Navigator>
 	);
