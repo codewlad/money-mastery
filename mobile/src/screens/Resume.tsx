492afb8393e2119d54545dab84b42943e6910a9c
Wladimir Ferreira Gonçalves <codewlad@gmail.com>
2023-10-07 20:32:37 -0300

    Updated Resume screen

--- mobile/src/screens/Resume.tsx
+++ mobile/src/screens/Resume.tsx
@@ -1,15 +1,88 @@
-import { Text, VStack } from 'native-base';
+import { Text, VStack, Image, HStack } from 'native-base';
 
 import { Header } from '@components/Header';
 
+import BackgroundImg from '@assets/background.png';
+
 export function Resume() {
 	return (
-		<VStack flex={1}>
-			<Header
-				variant='default'
-				title={'Resumo'}
+		<>
+			<Image
+				source={BackgroundImg}
+				defaultSource={BackgroundImg}
+				alt='Gráfico de linha'
+				resizeMode='contain'
+				position={'absolute'}
+				opacity={50}
+				mt={5}
 			/>
-			<Text>Resumo</Text>
-		</VStack>
+
+			<VStack
+				flex={1}
+				space={8}
+				alignItems={'center'}
+			>
+				<Header
+					variant='default'
+					title={'Resumo'}
+				/>
+				<Text
+					fontSize={'md'}
+					fontFamily={'heading'}
+					color={'white'}
+					textAlign={'center'}
+				>
+					Confira o saldo mensal
+				</Text>
+
+				<VStack
+					space={3}
+					px={5}
+					w={'full'}
+				>
+					<HStack space={4}>
+						<Text
+							color={'amber.400'}
+							fontSize={'xs'}
+							fontFamily={'heading'}
+							w={14}
+							textAlign={'center'}
+						>
+							Ano
+						</Text>
+						<Text
+							color={'amber.400'}
+							fontSize={'xs'}
+							fontFamily={'heading'}
+							flex={1}
+							textAlign={'center'}
+						>
+							Mês
+						</Text>
+						<Text
+							color={'amber.400'}
+							fontSize={'xs'}
+							fontFamily={'heading'}
+							flex={1}
+							textAlign={'center'}
+						>
+							Saldo
+						</Text>
+						<Text
+							color={'amber.400'}
+							fontSize={'xs'}
+							fontFamily={'heading'}
+							w={14}
+							textAlign={'center'}
+						>
+							Detalhes
+						</Text>
+					</HStack>
+
+					<Text color={'white'}>1</Text>
+					<Text color={'white'}>2</Text>
+				</VStack>
+			</VStack>
+		</>
 	);
 }
